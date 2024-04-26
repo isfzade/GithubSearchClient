c0cb8cd5216ebc9cd2a0af336ffc93019b0b4060
isfzade <irashidzade@gmail.com>
2024-04-26 17:06:29 +0400

    nothing to show to search

--- app/src/main/java/az/isfan/test3205/screens/search/components/ItemsInSearch.kt
+++ app/src/main/java/az/isfan/test3205/screens/search/components/ItemsInSearch.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import az.isfan.test3205.R
 import az.isfan.test3205.data.models.RepoData
 import az.isfan.test3205.general.Cavab
@@ -34,11 +38,22 @@ fun ItemsInSearch(
             )
         }
 
-        if (repos is Cavab.Success) {
+        if (repos is Cavab.Success && repos.data.isNotEmpty()) {
             items(count = repos.data.size) { index ->
                 ItemInRepo(
                     repo = repos.data[index],
                     onDownloadButtonClick = onDownloadButtonClick,
+                )
+            }
+        }
+        else {
+            item {
+                Text(
+                    text = stringResource(R.string.nothing_to_show),
+                    textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 20.sp
                 )
             }
         }
